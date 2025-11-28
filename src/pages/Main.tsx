e05d005c9c880dd9786cc7bd2bcfe6cfe3c50aba
Anton <komogorcevanton10@gmail.com>
2025-11-28 19:43:14 +0800

    feat: added a about section in the Main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,14 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-import heroImage from '../assets/pages/main/illustration.png'
-import ellipse from '../assets/pages/main/ellipse.svg'
+import heroImage from '../assets/pages/main/hero/illustration.png'
+import ellipse from '../assets/pages/main/hero/ellipse.svg'
+
+import women from '../assets/pages/main/about/women.png'
+import check from '../assets/pages/main/about/check.svg'
 
 import '../styles/pages/main/hero.css'
+import '../styles/pages/main/about.css'
 
 const Main = () => {
     return (
@@ -54,6 +58,53 @@ const Main = () => {
                             <p className='statistic-body'>Years of experience</p>
                         </div>
                     </article>
+                </section>
+
+                <section className='about'>
+                    <article className='about-column-one'>
+                        <img src={ women } alt="" />
+                    </article>
+
+                    <article className='about-column-two'>
+                        <section className='about-heading'>
+                            <h6 style={{ fontWeight: 'bold' }}>WHO WE ARE</h6>
+                            <h2 style={{ fontSize: '46px', fontWeight: '900' }}>Why Createx?</h2>
+                        </section>
+
+                        <section className='bulleted-list'>
+                            <div className='bulleted-list-items'>
+                                <img src={ check } alt="" />
+                                <p className='bulleted-items-body'>A fermentum in morbi pretium aliquam adipiscing <br /> donec tempus.</p>
+                            </div>
+
+                            <div className='bulleted-list-items'>
+                                <img src={ check } alt="" />
+                                <p className='bulleted-items-body'>Vulputate placerat amet pulvinar lorem nisl.</p>
+                            </div>
+
+                            <div className='bulleted-list-items'>
+                                <img src={ check } alt="" />
+                                <p className='bulleted-items-body'>Consequat feugiat habitant gravida quisque elit <br /> bibendum id adipiscing sed.</p>
+                            </div>
+
+                            <div className='bulleted-list-items'>
+                                <img src={ check } alt="" />
+                                <p className='bulleted-items-body'>Etiam duis lobortis in fames ultrices commodo nibh.</p>
+                            </div>
+
+                            <div className='bulleted-list-items'>
+                                <img src={ check } alt="" />
+                                <p className='bulleted-items-body'>Tincidunt sagittis neque sem ac eget.</p>
+                            </div>
+
+                            <div className='bulleted-list-items'>
+                                <img src={ check } alt="" />
+                                <p className='bulleted-items-body'>Ultricies amet justo et eget quisque purus <br /> vulputate dapibus tortor.</p>
+                            </div>
+                        </section>
+
+                        <button className='more-about-button'>More about us</button>
+                    </article>   
                 </section>
             </main>
 
